750d35fe818b7868293a8fc9c76afe8636ee43e9
Michael Angelo <55844504+michaelangrivera@users.noreply.github.com>
2022-06-30 16:39:35 -0400

    more

--- apps/server/src/index.ts
+++ apps/server/src/index.ts
@@ -17,25 +17,3 @@ createNestServer(server)
   .catch(err => console.error('Nest broken', err))
 export const api: functions.HttpsFunction = functions.https.onRequest(server)
 
-
-// // Import the functions you need from the SDKs you need
-// import { initializeApp } from "firebase/app"
-// import { getAnalytics } from "firebase/analytics"
-// // TODO: Add SDKs for Firebase products that you want to use
-// // https://firebase.google.com/docs/web/setup#available-libraries
-
-// // Your web app's Firebase configuration
-// // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-// const firebaseConfig = {
-//   apiKey: "AIzaSyBSaDwwJ8HKgDdTSxAFaBVEkGF1qTiwG0Y",
-//   authDomain: "bagelhouse-mileaday.firebaseapp.com",
-//   projectId: "bagelhouse-mileaday",
-//   storageBucket: "bagelhouse-mileaday.appspot.com",
-//   messagingSenderId: "401817550036",
-//   appId: "1:401817550036:web:d6300478813465fbb30f41",
-//   measurementId: "G-R8WWK5H31Q"
-// }
-
-// // Initialize Firebase
-// const app = initializeApp(firebaseConfig)
-// const analytics = getAnalytics(app)
