4345437ef8d0e187412970dda0dab236536ebe4e
Michael Angelo <55844504+michaelangrivera@users.noreply.github.com>
2022-07-03 12:08:12 -0400

    more

--- apps/next/pages/index.tsx
+++ apps/next/pages/index.tsx
@@ -1,3 +1,44 @@
 import { HomeScreen } from 'app/features/home/screen'
 
-export default HomeScreen
+import React, { useEffect } from 'react'
+import { getAuth, onAuthStateChanged } from "firebase/auth"
+import {
+    useAuthUser,
+    withAuthUser,
+    withAuthUserTokenSSR,
+    AuthUserContext
+  } from 'next-firebase-auth'
+
+const auth = getAuth()
+let uid = ''
+onAuthStateChanged(auth, (user) => {
+if (user) {
+    // User is signed in, see docs for a list of available properties
+    // https://firebase.google.com/docs/reference/js/firebase.User
+    uid = user.uid
+    // ...
+} else {
+    // User is signed out
+    // ...
+}
+})
+const Home = () => {
+    const AuthUser = useAuthUser()
+
+    useEffect(() => {
+      async function logToken(user: AuthUserContext) {
+          console.log(await user.getIdToken())
+          console.log('angelo ', uid)
+        }
+      
+       logToken(AuthUser)
+    }, [AuthUser])
+    return (
+        <>
+        <HomeScreen isAuthenticated = {AuthUser.email ? true : false}
+            userEmail = {AuthUser.email ? AuthUser.email : ''}/>
+        </>
+    )
+}
+
+export default Home
--- packages/app/features/home/screen.tsx
+++ packages/app/features/home/screen.tsx
@@ -1,62 +1,74 @@
 import { Text, useSx, View, H1, P, Row, A } from 'dripsy'
 import { TextLink } from 'solito/link'
 import { MotiLink } from 'solito/moti'
+import { MotiView } from 'moti'
+
+interface HomePageState {
+  isAuthenticated: boolean,
+  userEmail: string
+}
 
 
-export function HomeScreen() {
+export function HomeScreen(props: HomePageState) {
   const sx = useSx()
 
   return (
     <View
       sx={{ flex: 1, justifyContent: 'center', alignItems: 'center', p: 16 }}
     >
-      <H1 sx={{ fontWeight: '800' }}>Mile a day</H1>
+      <H1 sx={{ fontWeight: '800' }}>Mile a Day</H1>
       <View sx={{ maxWidth: 600 }}>
         <P sx={{ textAlign: 'center' }}>
-          Here is a basic starter to show you how you can navigate from one
-          screen to another. This screen uses the same code on Next.js and React
-          Native.
+          Keep track of your run streak and more.
         </P>
         <P sx={{ textAlign: 'center' }}>
-          Solito is made by{' '}
-          <A
-            href="https://twitter.com/fernandotherojo"
-            // @ts-expect-error react-native-web only types
-            hrefAttrs={{
-              target: '_blank',
-              rel: 'noreferrer',
-            }}
-            sx={{ color: 'blue' }}
-          >
-            Fernando Rojo
-          </A>
-          .
+          Coming soon.
         </P>
       </View>
       <Row>
-        <View sx={{ width: 32 }} />
-        <MotiLink
-          href="/auth"
-          animate={({ hovered, pressed }) => {
-            'worklet'
+        <View sx={{ alignItems: 'center' }} />
+        {props.isAuthenticated ?
+          <MotiView from={{
+            translateX: -105,
+          }}
+            animate={{
+              translateX: 0,
+            }}
+            transition={{
+              loop: true,
+              type: 'timing',
+              duration: 1500,
+              delay: 100,
+            }}>
+            <Text
+              selectable={false}
+              sx={{ fontSize: 16, color: 'black', fontWeight: 'bold' }}>
+              Nice! You&apos;re on the waiting list.
+            </Text>
+          </MotiView> :
+          <MotiLink
+            href="/auth"
+            animate={({ hovered, pressed }) => {
+              'worklet'
 
-            return {
-              scale: pressed ? 0.95 : hovered ? 1.1 : 1,
-              rotateZ: pressed ? '0deg' : hovered ? '-3deg' : '0deg',
-            }
-          }}
-          transition={{
-            type: 'timing',
-            duration: 150,
-          }}
-        >
-          <Text
-            selectable={false}
-            sx={{ fontSize: 16, color: 'black', fontWeight: 'bold' }}
+              return {
+                scale: pressed ? 0.95 : hovered ? 1.5 : 1,
+                rotateZ: pressed ? '0deg' : hovered ? '-3deg' : '0deg',
+              }
+            }}
+            transition={{
+              type: 'timing',
+              duration: 150,
+            }}
           >
-            Moti Link
-          </Text>
-        </MotiLink>
+            <Text
+              selectable={false}
+              sx={{ fontSize: 16, color: 'black', fontWeight: 'bold' }}
+            >
+              Sign up for the wait list!
+            </Text>
+          </MotiLink>
+        }
       </Row>
     </View>
   )
